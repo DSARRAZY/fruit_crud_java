7dfa59d1b50998d1a76facc70b5d163c6084ed06
DSARRAZY <d.sarrazy@gmail.com>
2021-06-03 15:24:29 +0200

    ajout scanner

--- src/com/cefim/fruit/Main.java
+++ src/com/cefim/fruit/Main.java
@@ -1,54 +1,55 @@
 package com.cefim.fruit;
 
-import java.sql.*;
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
+import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) throws SQLException {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        FruitJdbcDAO fruitJdbcDAO = new FruitJdbcDAO();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        System.out.println("Hello ! Quel est le nom du fruit à ajouter ?");
+        String fruitName = scanner.nextLine();
+        System.out.println("Date d'expiration ?");
+        LocalDate localDate = LocalDate.parse(scanner.nextLine(), formatter);
+
+        Fruit fruitToCreate = new Fruit(fruitName, localDate);
+        Fruit fruitCreated1 = fruitJdbcDAO.create(fruitToCreate);
+        System.out.println("Le fruit avec l'id " + fruitCreated1.getId() + " a été ajouté");
 
         // REQUETE READ
-       FruitJdbcDAO fruitJbdcDao = new FruitJdbcDAO();
-        List<Fruit> fruitList = fruitJbdcDao.findAll();
-
+        List<Fruit> fruitList = fruitJdbcDAO.findAll();
         for (Fruit f: fruitList) {
             System.out.println("ID " + f.getId() + " Fruit : " + f.getName() + " Date exp : " + f.getExpirationDate());
         }
 
-        // REQUETE CREATE
+//        // REQUETE CREATE
         Fruit fruit = new Fruit("Kiwi", LocalDate.now());
-
-        Fruit fruitCreated = fruitJbdcDao.create(fruit);
+        Fruit fruitCreated = fruitJdbcDAO.create(fruit);
         System.out.println("Le nouveau fruit avec l'id " + fruitCreated.getId()+ " a été ajouté");
 
         // REQUETE UPDATE
         fruitCreated.setName("UPDATED FRUIT");
-        boolean updateOk = fruitJbdcDao.update(fruitCreated);
+        LocalDate modifiedDate = LocalDate.of(2021, 07, 16);
+        fruitCreated.setExpirationDate(modifiedDate);
+
+        boolean updateOk = fruitJdbcDAO.update(fruitCreated);
         if (updateOk) {
-            System.out.println("le fruit avec id : " + fruitCreated.getId() + "a été MAJ");
+            System.out.println("Le fruit avec l'id " + fruitCreated.getId() + "a été MAJ");
         }
 
         // REQUETE DELETE
-        fruitJbdcDao.delete(13L);
+        fruitJdbcDAO.delete(13L);
 
+        //        boolean deleteOk = fruitJdbcDAO.delete(fruitCreated.getId());
+//        if (deleteOk) {
+//            System.out.println("Le fruit avec l'id " + fruitCreated.getId() + "a été supprimé");
+//        }
 
         ConnectionManager.closeConnection();
-
-
-
-
-//        // REQUETE 2
-//        String querySql = "SELECT * FROM fruit WHERE id = ?";
-//        try (PreparedStatement preparedStatement = connection.prepareStatement(querySql)) {
-//            preparedStatement.setLong(1, 10L);
-//            ResultSet resultSet = preparedStatement.executeQuery();
-//            resultSet.next();
-//            System.out.println(resultSet.getInt("id"));
-//            System.out.println(resultSet.getString("name"));
-//        } catch (SQLException e) {
-//            e.printStackTrace();
-//        }
 
 
     }
